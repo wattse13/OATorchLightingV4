a50e13a2b2c80e091971ea704455097b34ee323c
Eli Watts <eliwatts13@gmail.com>
2021-07-27 11:31:16 +0200

    InspectMenuController
    Added basic functionality

--- Assets/Scripts/InspectMenuController.cs
+++ Assets/Scripts/InspectMenuController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Attatched to InspectMenuController GameObject
 public class InspectMenuController : MonoBehaviour
 {
-    private Transform equipTransform;
+    private Vector2 centerTransform = new Vector2(0, 0);
     private GameObject currentPrefab;
+    // private GameObject prefabReturn;
 
     public GameObject useButton;
     public GameObject replaceButton;
@@ -19,32 +21,42 @@ public class InspectMenuController : MonoBehaviour
         GameEvents.OnMessageSent -= SetCurrentPrefab;
     }
    
+    // This method is called once a the OnMessageSent delegate event is triggered
     public void SetCurrentPrefab(GameObject myClickedPrefab)
     {
-        // Debug.Log(equipment.Name);
-        GetCurrentPrefab(myClickedPrefab);
+        currentPrefab = myClickedPrefab;
     }
 
-    public GameObject GetCurrentPrefab(GameObject myClickedPrefab)
+    // When this function is called by SetCurrentPrefab it should retrun the passed in GameObject parameter as a value
+    public GameObject GetCurrentPrefab()
     {
-        myClickedPrefab = currentPrefab;
-        Debug.Log("Made it to GetCurrentPrefab");
+        // Debug.Log("Made it to GetCurrentPrefab");
         return currentPrefab;
     }
 
-    public void CenterPrefab(GameObject currentPrefab)
+    // Assigns currentPrefab variable to value of the GetCurrentPrefab() return value
+    // Calls CenterPrefab() function and passes currentPrefab in as argument
+    public void CallCP()
     {
-        this.currentPrefab = currentPrefab;
+        currentPrefab = GetCurrentPrefab();
+        CenterPrefab(currentPrefab);
+    }
+
+    // This function should be called with a button OnClick method
+    public void CenterPrefab(GameObject myClickedPrefab)
+    {
         if (currentPrefab.TryGetComponent(out EquipmentClass equipment))
         {
-            // currentPrefab = myClickedPrefab;
+            // Check where object currently is
+            if(equipment.InitialPosition != centerTransform)
+            {
+                // Need to store original position value?
+                equipment.InitialPosition = centerTransform;
+            }
+            // Move if not centered
+            // Change layer order
+            // Enable alternate background (should a different script handle this?)
             Debug.Log(equipment.Name);
-            Debug.Log("Hi CurrentPrefab");
         }
-        // GetCurrentPrefab(currentPrefab);
-        // Debug.Log(currentPrefab.Name); // Scope issue
-        // equipTransform = equipment.GetComponent<Transform>();
-        // equipTransform.position = new Vector2(0, 0);
-        // equipment.InitialPosition = new Vector2(0, 0); // But how does it move back?
     }
 }
